71aaac64d305e8044a9d51a1ddcf3a8ba25ac8c7
mranbu <mranbu2003@gmail.com>
2025-02-14 21:31:01 -0800

    Event-Booking

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'header', component:HeaderComponent},
+    {path:'footer', component:FooterComponent},
+    {path:'', component:HomeComponent},
+    {path:'login', component:LoginComponent},
+    {path:'register',component:RegisterComponent}
+
+];
